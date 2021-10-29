f80b05cc206593c72adf79ea77d14369dd81ca8a
Veikka Aho <vhjaho@gmail.com>
2021-10-29 09:45:15 +0300

    khyl

--- dom.js
+++ dom.js
@@ -1,9 +1,10 @@
 // Kaikki elementit, joilla on sulkemisnappi katoavat näkyvistä painettaessa nappia
 
-var close = document.getElementsByClassName("close");
+var sulje = document.getElementsByClassName("sulje");
 var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
+// Käydään läpi kaikki elementit, joilla on sulkemisnappi ja annetaan niille funktio, joka piilottaa ne painettaessa
+for (i = 0; i < sulje.length; i++) {
+    sulje[i].onclick = function () {
         var div = this.parentElement;
         div.style.display = "none";
     }
@@ -11,7 +12,7 @@ for (i = 0; i < close.length; i++) {
 
 function uusi() {
 
-    // Luodaan uusi listaus syötekentän pohjalta, mikäli syöte ei ole tyhjä
+    // Luodaan uusi listaus syötekentän pohjalta
 
     // Luodaan ensin checkbox listaukselle
     var checkBox = document.createElement("input")
@@ -20,10 +21,11 @@ function uusi() {
     var li = document.createElement("li");
     var x = document.getElementById("syote").value;
     var y = document.createTextNode(x);
-    // Liitetään syöte ja checkbox listaukseen
+    // Liitetään syöte ja checkbox uuteen listaukseen
     li.appendChild(y);
     li.appendChild(checkBox);
 
+    // Tarkistetaan syötteen pituus
     if (x.length < 3) {
         document.getElementById("syote").placeholder = "Syötteen täytyy olla vähintään kolmen merkin pituinen..";
         alert("Syöte on liian lyhyt.");
@@ -40,16 +42,18 @@ function uusi() {
     document.getElementById("syote").value = "";
 
     // Luodaan listauksille sulkemisnappi
+    // Tehdään uusi span-elementti ja ruksi
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
 
-    span.className = "close";
+    // Annetaan span-elementille sulje-luokka ja liitetään span-elementti ja ruksi yhteen, jonka jälkeen ne liitetään uuteen lista-elementtiin
+    span.className = "sulje";
     span.appendChild(txt);
     li.appendChild(span);
 
     // Liitetään sulkemisnappi kaikkiin uusiin listauksiin
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
+    for (i = 0; i < sulje.length; i++) {
+        sulje[i].onclick = function () {
             var div = this.parentElement;
             div.style.display = "none";
         }
